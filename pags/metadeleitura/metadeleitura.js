fe09cf10610d002963d9d75408978fb41db1dd7b
Leonardo Costa Ferreira <leonardo_costaferreira22@hotmail.com>
2025-05-07 16:40:25 -0300

    atualizacao livraria GT

--- pags/metadeleitura/metadeleitura.js
+++ pags/metadeleitura/metadeleitura.js
@@ -12,51 +12,51 @@ searchButton.addEventListener('click', (event) => {
 })
 
 function fetchBooks(query) {
-    console.log('1')
     fetch(`https://openlibrary.org/search.json?q=${encodeURIComponent(query)}&language=por`)
-        .then(res => res.json())
-        .then(dados => {
-            console.log('2')
-            const livrosFiltrados = dados.docs.filter(item =>
-                item.language.includes('por') && !item.language.includes('eng')
-            ).slice(0, 10);
+    .then(res => res.json())
+    .then(dados => {
+        const livrosFiltrados = dados.docs.filter(item => 
+        item.language.includes('por') && !item.language.includes('eng')
+        ).slice(0,10);
 
-            if (livrosFiltrados.length === 0) {
-                booksContainer.innerHTML = '<h1>Nenhum livro encontrado</h1>'
-                return
-            }
-            booksContainer.innerHTML = livrosFiltrados.map(item => {
-                const urlImagem = `https://covers.openlibrary.org/b/id/${item.cover_i}-M.jpg`
-                return `
-                <div class="div-card">
-                    <div class="div-img">
-                        <img src='${urlImagem}' alt="capa do livro">
-                        <p class="tag-p">frete grátis</p>
-                    </div>
-                    <div class="div-texto">
-                        <div class="div-titulo">
-                            <h1 class=" truncate text-xs font-bold w-full">${item.title}</h1>
-                        </div>
-                        <h2 class="my-2 ">${item.author_name}</h2>
-                        <button onclick="adiocionarLivros${urlImagem}" class=" rounded-md flex justify-center w-full h-6 p-1 border border-blue-500"><i class=" text-blue-500 fa-solid fa-plus "></i></button>
-                    </div>
+        if (livrosFiltrados.length === 0) {
+            booksContainer.innerHTML = '<h1>Nenhum livro encontrado</h1>'
+            return
+        }
+        
+        booksContainer.innerHTML = livrosFiltrados.map(item => {
+            const urlImagem = `https://covers.openlibrary.org/b/id/${item.cover_i}-M.jpg`
+        return `
+            <div class="div-card">
+                <div class="div-img">
+                    <img src='${urlImagem}' alt="capa do livro">
+                    <p class="tag-p">frete grátis</p>
                 </div>
+                <div class="div-texto">
+                    <div class="div-titulo">
+                        <h1 class="truncate text-xs font-bold w-full">${item.title}</h1>
+                    </div>
+                    <h2 class="my-2">${item.author_name}</h2>
+                    <button onclick="adicionarLivro('${urlImagem}')" class="rounded-md flex justify-center w-full h-6 p-1 border border-blue-500">
+                    <i class="text-blue-500 fa-solid fa-plus"></i>
+                    </button>
+                </div>
+            </div>
+        
         `}).join('')
-        })
-        .catch(erro => {
-            console.log('Erro ao buscar livros: ', erro)
-            booksContainer.innerHTML = '<p> erro ao buscar livros</p>'
-        })
-    console.log('3')
+    })
+    .catch( erro => {
+        console.log('Erro ao buscar livros:', erro)
+        booksContainer.innerHTML = '<p>Erro ao buscar livros</p>'
+    })
 }
 
-function adicionarLIvro(capa) {
+function adicionarLivro (capa) {
     metaLivros.innerHTML += `
-       <div class="div-card w-[60px]">
-          <div class="div-img">
-            <img src='${capa}' alt="capa do livro" class"w-[150px] h-auto">
-         </div>
-        </div>
-    `
+        <div class="div-card w-[60px]">
+            <div class="div-img">
+                <img src='${capa}' alt="capa do livro" class="w-[150px] h-auto">
+            </div>
+        </div> 
+    ` 
 }
-
